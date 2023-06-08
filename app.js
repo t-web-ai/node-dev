d2ec6b2b44980447d3d433f6863228d534355e87
t-web-ai <118815589+t-web-ai@users.noreply.github.com>
2023-06-08 22:21:10 +0630

    Update app.js
    create server with http

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+var x = require("http");
+x.createServer(function(req, res){
+    res.write("Hello World");
+    res.end();
+}).listen(80, ()=>{
+    console.log("Server is listening on port 80");
+});
