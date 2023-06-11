71564dc393fccad3c40444e8ef1bafcc75b2639f
t-web-ai <118815589+t-web-ai@users.noreply.github.com>
2023-06-11 21:57:09 +0630

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 var http = require("http");
 
 var server = http.createServer(function(req, res){
-    fs.readFile("/api.txt", "utf-8", function(err, data){
+    fs.readFile("./api.txt", "utf-8", function(err, data){
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write(data);
         res.end();
