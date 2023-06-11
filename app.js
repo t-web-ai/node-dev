2e0237cfb5820a1b5b029e7851a0dd3f77991644
t-web-ai <118815589+t-web-ai@users.noreply.github.com>
2023-06-11 12:20:00 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var x = require("http");
-x.createServer(function(req, res){
-    res.write("Hello, Toee Thiha Kyaw");
-    res.end();
-}).listen(80, ()=>{
-    console.log("Server is listening on port 80");
+var fs = require("fs");
+var http = require("http");
+
+var server = http.createServer(function(req, res){
+    fs.readFile("/api.txt", "utf-8", function(err, data){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write(data);
+        res.end();
+    });
 });
