c70df7a83d64f52b7e0a023f8daa97976490c3f8
t-web-ai <118815589+t-web-ai@users.noreply.github.com>
2023-07-17 21:24:00 +0630

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = new express();
 const nodemailer = require("nodemailer");
 
-const num = [0,1,2,3,4,5,6,7,8,9];
+const num = [0,"A",1,"B",2,"C",3,"D",4,"E",5,"F",6,"G",7,"H",8,"X",9,"Z"];
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
